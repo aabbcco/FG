d160bd64bf9585cda554b88dc2739136fa70cc43
aabbcco <fooly_big_gun@qq.com>
2021-05-28 21:52:45 +0800

    work start

--- bot.py
+++ bot.py
@@ -4,16 +4,56 @@
 @time: created on 2020/4/4 14:57
 @修改记录:
 '''
-import os
-from Utils.JsonUtils import JsonUtils
-import nonebot
-import config
+# import os
+# from Utils.JsonUtils import JsonUtils
+# import nonebot
+# import config
+# from nonebot import require
+# scheduler = require('nonebot_plugin_apscheduler').scheduler
 
-if __name__ == '__main__':
-    configuration = JsonUtils.json2Dict(os.path.join(os.getcwd(), 'cn', 'acmsmu', 'FG', 'data', 'config.json'))
-    nonebot.init(config)
-    nonebot.load_plugins(
-        os.path.join(os.path.dirname(__file__), 'cn', 'acmsmu'),
-        'cn.acmsmu'
+# if __name__ == '__main__':
+#     configuration = JsonUtils.json2Dict(os.path.join(os.getcwd(), 'cn', 'acmsmu', 'FG', 'data', 'config.json'))
+#     nonebot.init(config)
+#     nonebot.load_plugins(
+#         os.path.join(os.path.dirname(__file__), 'cn', 'acmsmu'),
+#         'cn.acmsmu'
+#     )
+#     nonebot.run(host=configuration['nonebotHost'], port=configuration['nonebotPort'])
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import nonebot
+from nonebot.adapters.gocq import Bot as GoCqBot
+
+# Custom your logger
+#
+# from nonebot.log import logger, default_format
+# logger.add("error.log",
+#            rotation="00:00",
+#            diagnose=False,
+#            level="ERROR",
+#            format=default_format)
+
+# You can pass some keyword args config to init function
+nonebot.init()
+app = nonebot.get_asgi()
+
+driver = nonebot.get_driver()
+driver.register_adapter("cqhttp", GoCqBot)
+
+nonebot.load_builtin_plugins()
+nonebot.load_from_toml("pyproject.toml")
+nonebot.load_plugins("cn/acmsmu/FG")
+nonebot.load_plugin("nonebot_plugin_test")
+# Modify some config / config depends on loaded configs
+#
+# config = driver.config
+# do something...
+
+
+if __name__ == "__main__":
+    nonebot.logger.warning(
+        "Always use `nb run` to start the bot instead of manually running!"
     )
-    nonebot.run(host=configuration['nonebotHost'], port=configuration['nonebotPort'])
+    nonebot.run(app="__mp_main__:app")
--- cn/acmsmu/FG/__init__.py
+++ cn/acmsmu/FG/__init__.py
@@ -7,14 +7,20 @@
 import nonebot
 import time
 import os
+
+from nonebot.plugin import on_message
 from cn.acmsmu.FG import Timer
 from Utils.JsonUtils import JsonUtils
 from Utils.IOUtils import IOUtils
+from nonebot.typing import T_State
+from nonebot.adapters.cqhttp import Bot, Event
 
-configuration = JsonUtils.json2Dict(os.path.join(os.getcwd(),'cn','acmsmu','FG','data','config.json'))
+configuration = JsonUtils.json2Dict(os.path.join(
+    os.getcwd(), 'cn', 'acmsmu', 'FG', 'data', 'config.json'))
 groupInfo = configuration['groupInfo']
 for each in groupInfo:
-    fpath = os.path.join(os.getcwd(),'cn','acmsmu','FG','data',each['groupId'])
+    fpath = os.path.join(os.getcwd(), 'cn', 'acmsmu',
+                         'FG', 'data', each['groupId'])
     try:
         dataDict = dict()
         dataDict['flag'] = True
@@ -23,24 +29,30 @@ for each in groupInfo:
         IOUtils.serializeObj2Pkl(dataDict, fpath + '/var.pkl')
     except FileExistsError:
         continue
-bot = nonebot.get_bot()
-print('初始化完成')
 
-@bot.on_message('group')
-async def handleGroupMsg(session):
+wordcloud = on_message(block=False)
+
+
+@wordcloud.handle()
+async def HandleGroupMsg(bot: Bot, event: Event):
     groupInfo = configuration['groupInfo']
-    for each in groupInfo:
-        if each['groupId'] == str(session['group_id']):
-            # 读取每个群文件夹的pkl
-            dataDict = IOUtils.deserializeObjFromPkl(os.path.join(os.getcwd(),'cn','acmsmu','FG','data',each['groupId'],'var.pkl'))
-            # 确定flag的值
-            flag = dataDict['flag']
-            # 确定要往哪一个文件中写入聊天记录
-            msg = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + ' ' + str(session['user_id']) + '\n' + session['raw_message'] + '\n'
-            if flag:
-                with open(os.path.join(os.getcwd(),'cn','acmsmu','FG','data',each['groupId'],'chatA.txt'), 'a', encoding='utf-8') as fileA:
-                    fileA.write(msg)
-            else:
-                with open(os.path.join(os.getcwd(),'cn','acmsmu','FG','data',each['groupId'],'chatB.txt'), 'a', encoding='utf-8') as fileB:
-                    fileB.write(msg)
-            break
+
+
+# @bot.on_message('group')
+# async def handleGroupMsg(session):
+#     groupInfo = configuration['groupInfo']
+#     for each in groupInfo:
+#         if each['groupId'] == str(session['group_id']):
+#             # 读取每个群文件夹的pkl
+#             dataDict = IOUtils.deserializeObjFromPkl(os.path.join(os.getcwd(),'cn','acmsmu','FG','data',each['groupId'],'var.pkl'))
+#             # 确定flag的值
+#             flag = dataDict['flag']
+#             # 确定要往哪一个文件中写入聊天记录
+#             msg = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + ' ' + str(session['user_id']) + '\n' + session['raw_message'] + '\n'
+#             if flag:
+#                 with open(os.path.join(os.getcwd(),'cn','acmsmu','FG','data',each['groupId'],'chatA.txt'), 'a', encoding='utf-8') as fileA:
+#                     fileA.write(msg)
+#             else:
+#                 with open(os.path.join(os.getcwd(),'cn','acmsmu','FG','data',each['groupId'],'chatB.txt'), 'a', encoding='utf-8') as fileB:
+#                     fileB.write(msg)
+#             break
